c9c9bd2d5aa9bde4d13836399a6fb389c4582fe2
JonaSabido <jona_sabido@hotmail.com>
2024-07-10 11:02:30 -0600

    JS: new URI

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
     // urlApiBase: 'https://apirhxsp.onrender.com/api',
-    urlApiBase: 'http://localhost:3000/api',
-    // urlApiBase: 'http://10.0.2.96:3000/api',
-
+    //urlApiBase: 'http://localhost:3000/api',
+    urlApiBase: 'http://10.0.1.54:3000/api',
 };
